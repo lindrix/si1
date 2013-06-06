9e658a25d97930f30241e2ba1ecf50bb7fcddf7c
lindrix <lindrix@gmail.com>
2013-06-05 23:59:08 -0300

    projeto

---
+++ ufcg/si1/Lab01.java
@@ -0,0 +1,9 @@
+package ufcg.si1;
+public class Lab01{
+	public static String xpto(){
+		return "Mexendo com git e ant";
+}
+public static void main(String[] args){
+	System.out.println(xpto());
+}
+}
---
+++ ufcg/si1/TesteLab01.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+package ufcg.si1;
+public class TesteLab01{
+@Test 
+public void testaProduto(){
+Assert.assertEquals("Mexendo com git e ant", Lab01.xpto());
+}
+}
